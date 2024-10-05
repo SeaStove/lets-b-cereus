606ed9e3451b5e7a44cceee90d0a82ebacb93d8a
SeaStove <rochovian@gmail.com>
2024-10-05 19:58:52 -0400

    made a timer and game over screen

---
+++ scripts/level_timer.gd
@@ -0,0 +1,34 @@
+extends Timer
+
+# Declare variables
+var total_time = 60  # Total time in seconds for the level timer
+var remaining_time = total_time  
+@onready var timer_label: Label = $"../CanvasLayer/TimerLabel"
+@onready var game_over_label: Label = $"../CanvasLayer/GameOverLabel"
+
+
+# Called when the node is ready
+func _ready():
+	# Update the label at the start
+	update_timer_display()
+	# Start the timer for every second
+	start(1)  # The timer will tick every 1 second
+
+# Update the timer each time the timer "ticks"
+func _on_timeout():
+	remaining_time -= 1  # Decrease remaining time by 1 second
+	update_timer_display()
+
+	if remaining_time <= 0:
+		end_game()
+
+# Function to update the timer label
+func update_timer_display():
+	timer_label.text = str(remaining_time) + "s"
+
+# End game when the timer reaches 0
+func end_game():
+	stop()  # Stop the timer
+	timer_label.hide()  # Hide the timer label
+	game_over_label.text = "You Win!"  # Set the text to "You Win"
+	game_over_label.show()  # Show the You Win label
