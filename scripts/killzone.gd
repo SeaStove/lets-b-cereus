179fde2cb52af80893ef5f6ce02ef682e02bef86
Christian Stovall <seastove@gmail.com>
2024-10-05 23:36:48 -0400

    updated info

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,9 +3,9 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died!")
 	body.get_node("CollisionShape2D").queue_free()
 	if(body.is_in_group("Player")):
+		print("You died!")
 		timer.start()
 		Engine.time_scale = 0.5
 	else:
